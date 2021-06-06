4c5ee7111d85729cf4f6d04d334f31173360823a
Imo999 <rcajina.pro@gmail.com>
2021-06-06 09:32:48 +0200

    add token contract + test

---
+++ contracts/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IToken.sol";
+
+contract Token is ERC20, IToken {
+    address private _owner;
+
+    constructor(address owner_) ERC20("RoToken", "ROT") {
+        _owner = owner_;
+        _mint(owner_, 1000000 * 10**decimals());
+    }
+
+    function owner() public view override returns (address) {
+        return _owner;
+    }
+}

ec5c0a53c74eb0db11f6b068550ad96387acb363
Imo999 <rcajina.pro@gmail.com>
2021-06-07 09:53:35 +0200

    add Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./IToken.sol";
 
+ /// @title Create RoToken
+ /// @author RC
+
 contract Token is ERC20, IToken {
     address private _owner;
 
